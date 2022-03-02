12e8efd0a7aa23f71ed2eb9a7f4e1ca9b4565237
Luka Weis <lukat.weis@gmail.com>
2022-03-02 20:45:36 +0100

    initial commit

---
+++ example.ps1
@@ -0,0 +1,59 @@
+﻿configuration SSL
+{
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+    Import-DscResource -ModuleName xWebAdministration
+    Import-DscResource -Module xMakeCertificate
+
+    node ("localhost")
+    {
+        xMakeCertificate CACert {
+            Type                    = 'Root'
+            Ensure                  = 'Present'
+            CommonName              = 'CA'
+            Store                   = 'Cert:\LocalMachine\CA'
+            SubjectAlternativeNames = 'server.contoso.com'
+            ExportPath              = 'C:\ca.pfx'
+            PFXPassword             = 'Password'
+        }
+        
+        xMakeCertificate SSLCert {
+            Type                    = 'Web'
+            Ensure                  = 'Present'
+            CommonName              = 'Web'
+            Store                   = 'Cert:\LocalMachine\My'
+            SubjectAlternativeNames = 'server.contoso.com'
+            SignerPath              = 'C:\ca.pfx'
+            SignerPassword          = 'Passw0rd'
+            DependsOn               = '[xMakeCertificate]CACert'
+        }
+        WindowsFeature WebServer {
+            Ensure = "Present"
+            Name   = "Web-Server"
+        }
+
+        File WebContent {
+            Ensure          = 'Present'
+            SourcePath      = 'c:\sources\mysite'
+            DestinationPath = 'c:\intepub\mysite'
+            Recurse         = $true
+            Type            = 'Directory'
+            DependsOn       = '[xMakeCertificate]SSLCert'
+        }
+
+        xWebSite MySite {
+            Ensure       = 'Present'
+            Name         = 'MySite'
+            State        = 'Started'
+            PhysicalPath = 'c:\intepub\mysite'
+            BindingInfo  = @(
+                MSFT_xWebBindingInformation {
+                    Protocol             = 'HTTPS'
+                    Port                 = 443
+                    CertificateSubject   = 'CN=Web'
+                    CertificateStoreName = 'MY'
+                }
+            )
+            DependsOn    = @('[File]WebContent', '[WindowsFeature}WebServer', '[xMakeCertificate]SSLCert')
+        }
+    }
+}
---
+++ xMakeCertificate/1.1/xMakeCertificate.psd1
@@ -0,0 +1,41 @@
+﻿@{
+    RootModule           = 'xMakeCertificate.psm1'
+
+    # Die Versionsnummer dieses Moduls
+    ModuleVersion        = '1.1'
+
+    # ID zur eindeutigen Kennzeichnung dieses Moduls
+    GUID                 = '964953a4-886a-4fce-af06-f287b6869dd1'
+
+    # Autor dieses Moduls
+    Author               = 'Luka Weis'
+
+    # Unternehmen oder Hersteller dieses Moduls
+    CompanyName          = ''
+
+    # Urheberrechtserklärung für dieses Modul
+    Copyright            = ''
+
+    # Beschreibung der von diesem Modul bereitgestellten Funktionen
+    Description          = 'Creates and signs certificates'
+
+    # Die für dieses Modul mindestens erforderliche Version des Windows PowerShell-Moduls
+    PowerShellVersion    = '5.0'
+
+    # Aus diesem Modul zu exportierende Funktionen. Um optimale Leistung zu erzielen, verwenden Sie keine Platzhalter und löschen den Eintrag nicht. Verwenden Sie ein leeres Array, wenn keine zu exportierenden Funktionen vorhanden sind.
+    FunctionsToExport    = '*'
+
+    # Aus diesem Modul zu exportierende Cmdlets. Um optimale Leistung zu erzielen, verwenden Sie keine Platzhalter und löschen den Eintrag nicht. Verwenden Sie ein leeres Array, wenn keine zu exportierenden Cmdlets vorhanden sind.
+    CmdletsToExport      = '*'
+
+    # Die aus diesem Modul zu exportierenden Variablen
+    VariablesToExport    = '*'
+
+    # Aus diesem Modul zu exportierende Aliase. Um optimale Leistung zu erzielen, verwenden Sie keine Platzhalter und löschen den Eintrag nicht. Verwenden Sie ein leeres Array, wenn keine zu exportierenden Aliase vorhanden sind.
+    AliasesToExport      = '*'
+
+    # Aus diesem Modul zu exportierende DSC-Ressourcen
+    DscResourcesToExport = 'xMakeCertificate'
+
+}
+
